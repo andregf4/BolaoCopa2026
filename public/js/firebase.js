e0ac9a77b9400c6a296f3aa525540bed6758b65e
André Gerez Foratto <121778217+andregf4@users.noreply.github.com>
2026-05-25 17:23:30 -0300

    firebase.js protected

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAxbS2SBj0uqDTuSQBg_9ciFmxBcxu3VeE",
-  authDomain: "bolaocopa2026-2880d.firebaseapp.com",
-  projectId: "bolaocopa2026-2880d",
-  storageBucket: "bolaocopa2026-2880d.firebasestorage.app",
-  messagingSenderId: "842355163421",
-  appId: "1:842355163421:web:1107836d356fdd199aa1f4"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
